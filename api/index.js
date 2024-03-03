83827dec177c399e3fbafae676da8bb79cdefe3b
nashira27 <fathimanashira@gmail.com>
2024-03-03 10:29:17 +0530

    Created route and controller for user apis

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRoutes from "./routes/userRoute.js";
 
 const app = express();
 dotenv.config();
 app.listen(3000, () => console.log("Server listening on port 3000"));
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connected to db"))
   .catch((err) => console.log(err));
+
+app.use("/api/user", userRoutes);
