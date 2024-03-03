6025f303d002c840e4b96556ea9ee95642adf209
nashira27 <fathimanashira@gmail.com>
2024-03-03 09:58:48 +0530

    Added pages and Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className="bg-slate-400">
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <Link><h1 className="font-bold">Recipe Box</h1></Link>
+            <ul className="flex gap-4">  
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/profile'><li>Profile</li></Link>
+                <Link to='/sign-in'><li>Sign In</li></Link>
+                <Link to='/sign-up'><li>Sign Up</li></Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
