b2de98a1fd0d2f58c1df4a46a2d671d3065c841d
nashira27 <fathimanashira@gmail.com>
2024-03-03 10:47:00 +0530

    Added hashing for password

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,11 +1,21 @@
 import User from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, firstname, lastname, email, password } = req.body;
-  const newUser = new User({ username, firstname, lastname, email, password });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    firstname,
+    lastname,
+    email,
+    password: hashedPassword,
+  });
+  console.log(newUser);
   try {
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully." });
+    const addedUser = await newUser.save();
+    console.log(addedUser);
+    res.status(201).json(addedUser);
   } catch (error) {
     res.status(500).json(error);
   }
