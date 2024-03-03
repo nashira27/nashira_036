bec5745ed67cf3b83ad6a72d9cb6b37f38e3930b
nashira27 <fathimanashira@gmail.com>
2024-03-03 12:41:48 +0530

    Updated header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const {currentUser} = useSelector((state)=> state.user)
   return (
     <div className="bg-slate-700  text-black">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
             <Link><h1 className="font-bold">Recipe Box</h1></Link>
             <ul className="flex gap-4">  
                 <Link to='/'><li>Home</li></Link>
-                <Link to='/profile'><li>Profile</li></Link>
-                <Link to='/sign-in'><li>Sign In</li></Link>
-                <Link to='/sign-up'><li>Sign Up</li></Link>
+                { currentUser ? (
+                  <Link to='/profile'><li>Profile</li></Link>
+                  
+                  ):(
+                    <Link to='/sign-in'><li>Sign In</li></Link>
+                  )}
+                { currentUser && <Link to='/'><li>Sign Out</li></Link>}
             </ul>
         </div>
     </div>
